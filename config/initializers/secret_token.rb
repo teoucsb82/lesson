fc4e7887376bdcdc68d327d4a69b2464d0e8fd58
Teo Dell'Amico <teo.dellamico@gmail.com>
2014-10-04 09:49:51 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lesson::Application.config.secret_key_base = '12877446de4008aaacbc5a9edae62f9237d496b9af62039885cf61fc0a52f7ac0c998244489613ad2cb0398f3111fa796066e04762adaf00f590668e4ab2e20c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lesson::Application.config.session_store :cookie_store, key: '_lesson_session'
